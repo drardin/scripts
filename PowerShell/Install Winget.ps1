aff62edf2d11d907dcd89d1ff512ff3f2a7c2ef6
drardin <81466808+drardin@users.noreply.github.com>
2023-10-01 01:12:42 -0600

    Create Install Winget.ps1
    Initial commit

---
+++ PowerShell/Install Winget.ps1
@@ -0,0 +1,15 @@
+# This script installs Winget using Microsoft's recommended Powershell method
+
+# Check if installed
+$wingetInstalled = Get-Command winget -ErrorAction SilentlyContinue
+
+# If not, install Winget
+if (-not $wingetInstalled) {
+    Write-Host "Installing Windows Package Manager (winget)..."
+    Invoke-WebRequest -Uri https://aka.ms/getwinget -OutFile Microsoft.DesktopAppInstaller_8wekyb3d8bbwe.msixbundle
+    Invoke-WebRequest -Uri https://aka.ms/Microsoft.VCLibs.x64.14.00.Desktop.appx -OutFile Microsoft.VCLibs.x64.14.00.Desktop.appx
+    Invoke-WebRequest -Uri https://github.com/microsoft/microsoft-ui-xaml/releases/download/v2.7.3/Microsoft.UI.Xaml.2.7.x64.appx -OutFile Microsoft.UI.Xaml.2.7.x64.appx
+    Add-AppxPackage Microsoft.VCLibs.x64.14.00.Desktop.appx
+    Add-AppxPackage Microsoft.UI.Xaml.2.7.x64.appx
+    Add-AppxPackage Microsoft.DesktopAppInstaller_8wekyb3d8bbwe.msixbundle
+}
